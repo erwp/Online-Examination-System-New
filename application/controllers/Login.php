47d1e8d963567a980d8fcefe145d20b88bb711a0
erwp <pandithwahid37@gmail.com>
2021-09-07 19:30:26 -0700

    Login Controller

---
+++ application/controllers/Login.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+	public function index()
+	{
+		echo "<h2>Login Page</h2>";
+		echo "<h2>Login Page</h2>";
+	}
+}
